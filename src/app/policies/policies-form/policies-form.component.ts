eaa0bb4e079b127207749923b4311890c1ffa89d
codinci <dkibau@gmail.com>
2025-01-16 22:32:44 +0300

    initial commit

---
+++ src/app/policies/policies-form/policies-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies-form',
+  imports: [],
+  templateUrl: './policies-form.component.html',
+  styles: ``
+})
+export class PoliciesFormComponent {
+
+}
---
+++ src/app/policies/policies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PoliciesFormComponent } from "./policies-form/policies-form.component";
+
+@Component({
+  selector: 'app-policies',
+  imports: [PoliciesFormComponent],
+  templateUrl: './policies.component.html',
+  styles: ``
+})
+export class PoliciesComponent {
+
+}
